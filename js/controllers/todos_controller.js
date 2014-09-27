9dd7a75f839f8eb90d366d2359c6800971279803
neich8 <neich0888@gmail.com>
2014-09-27 14:29:47 -0500

    Added Crossout functionality

--- js/controllers/todos_controller.js
+++ js/controllers/todos_controller.js
@@ -1,18 +1,22 @@
-Todos.TodosController = Ember.ArrayCOntroller.extend({
-	actions: {
-		createTodo: function() {}
+Todos.TodosController = Ember.ArrayController.extend({
+  actions: {
+    createTodo: function () {
+      // Get the todo title set by the "New Todo" text field
+      var title = this.get('newTitle');
+      if (!title.trim()) { return; }
 
-		var title = this.get('newTitle');
-		if (!title) { return false; }
-		if (!title.trim()) { return; }
+      // Create the new Todo model
+      var todo = this.store.createRecord('todo', {
+        title: title,
+        isCompleted: false
+      });
 
-		var todo = this.store.createRecord('todo', {
-			title: title
-			isCompleted: false
-		});
+      // Clear the "New Todo" text field
+      this.set('newTitle', '');
 
-		this.set('newTitle', '');
+      // Save the new model
+      todo.save();
+    }
+  }
+});
 
-		todo.save();
-	}
-})
